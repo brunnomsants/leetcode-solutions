632a6486a733a5ac96c82aa932f09635d1a874ce
brunnomsants <brunnomurilo1@gmail.com>
2024-05-27 18:50:42 -0300

    Update roman to integer

--- roman-to-integer.py
+++ roman-to-integer.py
@@ -1,13 +1,32 @@
 class Solution:
-    def intToRoman(self, num: int) -> str:
-            values = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1]
-            romanvalues = ["M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV","I"]
-            result = ""
-            i = 0
-            while num>0 :
-               if(num>=values[i]):
-                  result+=romanvalues[i]
-                  num -= values[i]
-               else:
-                  i+=1
-            return result
+    def romanToInt(self, s: str) -> int:
+        romanvaluelist = list(s)
+        result = 0
+        lastsum = []
+        for i in range(len(romanvaluelist)):
+            if(romanvaluelist[i] == "M"):
+                result += 1000
+                lastsum.append(1000) 
+            if(romanvaluelist[i] == "D"):
+                result += 500
+                lastsum.append(500) 
+            if(romanvaluelist[i] == "C"):
+                result += 100
+                lastsum.append(100) 
+            if(romanvaluelist[i] == "L"):
+                result += 50
+                lastsum.append(50) 
+            if(romanvaluelist[i] == "X"):
+                result += 10
+                lastsum.append(10) 
+            if(romanvaluelist[i] == "V"):
+                result += 5
+                lastsum.append(5)
+            if(romanvaluelist[i] == "I"):
+                result += 1
+                lastsum.append(1) 
+            if(lastsum[i-1] < lastsum[i]):
+                result -= lastsum[i-1]*2
+        return result
+    
+print(Solution.romanToInt(0, "MCMXCIV"))
