516c035ec23069bbe0dad1e96c80edb3727f1b1c
brunnomsants <brunnomurilo1@gmail.com>
2024-08-10 23:52:07 -0300

    Update

--- longest-substring-without-repeating-characters.py
+++ longest-substring-without-repeating-characters.py
@@ -1,25 +1,5 @@
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-        if not s:
-            return 0
-        
-        max_length = 0
-        start = 0
-        char_index = {}
-        
-        for i, char in enumerate(s):
-            if char in char_index and char_index[char] >= start:
-                start = char_index[char] + 1
-            else:
-                max_length = max(max_length, i - start + 1)
-            char_index[char] = i
-        print(char_index["b"])
-        print(enumerate(s))
-        return max_length
-print(Solution.lengthOfLongestSubstring(0, "abcabcbb")) 
-
-class Solution:
-    def lengthLongestSubstring(self, s: str) -> int:
         max_length = 0
         word = ""
         for i in range(len(s)):
@@ -31,4 +11,4 @@ class Solution:
         max_length = max(max_length, len(word))
         return max_length
 
-print(Solution.lengthLongestSubstring(0, "pwwekw")) 
+print(Solution.lengthOfLongestSubstring(0, "pwwekw")) 
